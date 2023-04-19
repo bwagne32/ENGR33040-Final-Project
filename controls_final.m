2def55c677d1ef07d509f36c5303df10daef773a
bwagne32 <bwagne32@kent.edu>
2023-04-19 16:27:24 -0400

    Added comments and more verbosity

--- controls_final.m
+++ controls_final.m
@@ -1,17 +1,18 @@
+%%% Control Systems Final 
+% Josh Colvis, Kyle Morgan, Ben Wagner
+clear;
 %% Rlocus
 num = [1];
 den = [1 11 10 0];
 rlocus (num,den);
 sgrid;
-%%
+%% Step Plot
 figure;
 G = tf(num,den);
 step(G);
 
-%%
+%% Step Response Graphs and Gain Values
 % Define the plant transfer function
-num = [1];
-den = [1 11 10 0];
 G = tf(num, den);
 
 % Define the range of K values to test
@@ -30,7 +31,7 @@ for K = K_values
     % Compute the step response characteristics
     step_info = stepinfo(sys_cl);
 
-    % Check if the overshoot and maximum steering input constraints are satisfied
+    % Checking overshoot and maximum steering input
     if step_info.Overshoot <= 10 && step_info.Peak <= 4
         % If the constraints are satisfied, check if the rise time is the smallest so far
         if step_info.RiseTime < best_rise_time
@@ -42,11 +43,14 @@ end
 
 % Print the optimal K value and the corresponding rise time
 fprintf('Optimal K value: %.2f\n', best_K);
-fprintf('Rise time: %.2f\n', best_rise_time);
+fprintf('Rise time: %.2f s\n', best_rise_time);
 
 % Plot the step response of the closed-loop system with the optimal K value
 Gc_opt = tf(best_K, 1);
 sys_cl_opt = feedback(G*Gc_opt, 1);
 step(sys_cl_opt);
+fprintf("Max steering angle: %.2f degrees\n",step_info.Peak)
 
-
+%% Bode Plot
+figure
+bode(num,den);
