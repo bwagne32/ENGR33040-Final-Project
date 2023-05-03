d1b28b88513dfe7faf1496132a7fdc446298e021
bwagne32 <bwagne32@kent.edu>
2023-05-03 13:46:35 -0400

    Bandwidth calculations now work

--- controls_final.m
+++ controls_final.m
@@ -12,37 +12,33 @@ G = tf(num,den);
 step(G);
 
 %% Step Response Graphs and Gain Values
-% Gain values
+% Gain values to test
 K_values = 0:0.1:100;
 
-% Initialize variables to store the best K and corresponding rise time
 best_K = 0;
 best_rise_time = Inf;
 
-% Iterate over the K values and compute the step response characteristics
 for K = K_values
-    % Define the closed-loop transfer function with proportional control
     Gc = tf(K, 1);
-    sys_cl = feedback(G*Gc, 1);
+    sys_cl = feedback(G*Gc, 1); % closed-loop transfer function with proportional control
 
-    % Compute the step response characteristics
+    % stepinfo() function creates a data structure with calculations for
+    % overshoot and rise time needed later on
     step_info = stepinfo(sys_cl);
 
-    % Checking overshoot and maximum steering input
-    if step_info.Overshoot <= 10 && step_info.Peak <= 4
-        % If the constraints are satisfied, check if the rise time is the smallest so far
-        if step_info.RiseTime < best_rise_time
+    if step_info.Overshoot <= 10 && step_info.Peak <= 4 % checking if lane overshoot and steering angle are acceptable
+        if step_info.RiseTime < best_rise_time % Checking if new time is better
             best_rise_time = step_info.RiseTime;
             best_K = K;
         end
     end
 end
 
-% Print the optimal K value and the corresponding rise time
+% Printing statements for results
 fprintf('Optimal K value: %.2f\n', best_K);
 fprintf('Rise time: %.2f s\n', best_rise_time);
 
-% Plot the step response of the closed-loop system with the optimal K value
+% Step response plot with best gain (K) value found
 Gc_opt = tf(best_K, 1);
 sys_cl_opt = feedback(G*Gc_opt, 1);
 step(sys_cl_opt);
@@ -51,8 +47,13 @@ fprintf("Max steering angle: %.2f degrees\n",step_info.Peak)
 %% Bode Plot
 figure
 bode(num,den);
-fprintf("Bandwidth: %f\nCutoff: ",bandwidth(Gc))
-
-
-
+[mag, phase, wout] = bode(tf(num,den), logspace(-1,1,1000));
+magdb = 20*log10(mag);
+for i = 1:1000
+    if (ceil(magdb(1,1,i)) == -3)
+        bandwidth = wout(i);
+        break;
+    end
+end
+fprintf("Bandwidth: %.2f\n",bandwidth)
 
